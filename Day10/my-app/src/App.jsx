ed9c1701c3f47d1170a5baaee961f85ac0af5ca9
vky-fsd <vk206vignesh@gmail.com>
2023-08-25 13:07:06 +0530

    Day10

---
+++ Day10/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Axi from './Axi'
+
+function App() {
+
+  return (
+   <>
+  <Axi/>
+   </>
+  )
+}
+
+export default App
--- Day9.1/my-app/src/App.jsx
+++ Day9.1/my-app/src/App.jsx
@@ -1,19 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Ref from './Ref'
-import ToyStore from './ToyStore'
+import React, { useReducer } from 'react';
+import './App.css';
+
+// Reducer function to handle game state changes
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case 'RESET':
+      return { squares: Array(9).fill(null), xIsNext: true, winner: null };
+    case 'CLICK':
+      const squares = state.squares.slice();
+      if (calculateWinner(squares) || squares[action.index]) {
+        return state;
+      }
+      squares[action.index] = state.xIsNext ? 'X' : 'O';
+      return {
+        squares: squares,
+        xIsNext: !state.xIsNext,
+        winner: calculateWinner(squares),
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = { squares: Array(9).fill(null), xIsNext: true, winner: null };
+
+// Function to determine the winner
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(gameReducer, initialState);
+
+  const renderSquare = (i) => {
+    return (
+      <button className="square" onClick={() => dispatch({ type: 'CLICK', index: i })}>
+        {state.squares[i]}
+      </button>
+    );
+  };
+
+  const resetGame = () => {
+    dispatch({ type: 'RESET' });
+  };
+
+  const winner = state.winner;
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+  }
 
   return (
-    <>
-    
-     <ToyStore/>
-    </>
-  )
+    <div className="game">
+      <div className="game-board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <button onClick={resetGame}>Reset Game</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
