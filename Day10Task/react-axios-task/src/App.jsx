3152ef43abfe50445a13b770f18822bb89c574d1
vky-fsd <vk206vignesh@gmail.com>
2023-08-25 22:32:12 +0530

    Day10Task

---
+++ Day10Task/react-axios-task/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import User from './User'
+import Usermangement from './UserMangement'
+
+function App() {
+ 
+  return (
+    <>
+    <Usermangement/>
+    </>
+  )
+}
+
+export default App
---
+++ Day10Task/react-axios-task/src/UserMangement.jsx
@@ -0,0 +1,191 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function User() {
+  const baseURL = "https://jsonplaceholder.typicode.com/users";
+  const [posts, setPosts] = useState([]);
+  const [newUser, setNewUser] = useState({ name: "", username: "", email: "" , phone: "" , website: "" });
+  const [editingUser, setEditingUser] = useState(null);
+  const [originalEditingUser, setOriginalEditingUser] = useState(null);
+
+
+  
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+
+  const handleAddUser = () => {
+    axios.post(baseURL, newUser).then((response) => {
+      setPosts([...posts, response.data]);
+      setNewUser({ name: "", username: "", email: "", phone: "" , website: "" });
+    });
+  };
+
+  const handleEditUser = () => {
+    if (!editingUser) return;
+
+    axios.put(`${baseURL}/${editingUser.id}`, editingUser).then(() => {
+      const updatedPosts = posts.map((post) =>
+        post.id === editingUser.id ? editingUser : post
+      );
+      setPosts(updatedPosts);
+      setEditingUser(null);
+    });
+  };
+
+  const handleDeleteUser = (id) => {
+    axios.delete(`${baseURL}/${id}`).then(() => {
+      const updatedPosts = posts.filter((post) => post.id !== id);
+      setPosts(updatedPosts);
+    });
+  };
+  const cancelEdit = () => {
+    setEditingUser(null);
+    if (editingUser) {
+      // Reset editingUser back to its original state
+      setEditingUser(originalEditingUser);
+      
+    }
+    const handleEditUser = (user) => {
+        // Set originalEditingUser to store the original data
+        setOriginalEditingUser(user);
+        setEditingUser(user);
+      };
+      
+  };
+  
+  
+
+  const gridStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+    gap: "20px",
+  };
+
+  return (
+    <>
+    <div>
+      <h1>User Management</h1>
+      <br></br>
+      </div>
+      <div>
+         <div className="input">
+        <h2>Add User</h2>
+          <input
+            type="text"
+            placeholder="Name"
+            value={newUser.name}
+            onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            value={newUser.username}
+            onChange={(e) =>
+              setNewUser({ ...newUser, username: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            value={newUser.email}
+            onChange={(e) =>
+              setNewUser({ ...newUser, email: e.target.value })
+            }
+          />
+           <input
+            type="text"
+            placeholder="Phone Number"
+            value={newUser.phone}
+            onChange={(e) =>
+              setNewUser({ ...newUser, phone: e.target.value })
+            }
+          />
+           <input
+            type="text"
+            placeholder="Website"
+            value={newUser.website}
+            onChange={(e) =>
+              setNewUser({ ...newUser, website: e.target.value })
+            }
+          />
+          <button className="btn btn-primary" onClick={handleAddUser}>Submit</button>
+        </div>
+      </div>
+            <br></br>
+      <div>
+        
+        {editingUser && (
+          <div className="input">
+            <h2 id = "edit">Edit User</h2>
+            <input
+              type="text"
+              placeholder="Name"
+              value={editingUser.name}
+              onChange={(e) =>
+                setEditingUser({ ...editingUser, name: e.target.value })
+              }
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              value={editingUser.username}
+              onChange={(e) =>
+                setEditingUser({ ...editingUser, username: e.target.value })
+              }
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              value={editingUser.email}
+              onChange={(e) =>
+                setEditingUser({ ...editingUser, email: e.target.value })
+              }
+            />
+             <input
+              type="text"
+              placeholder="Phone"
+              value={editingUser.phone}
+              onChange={(e) =>
+                setEditingUser({ ...editingUser, phone: e.target.value })
+              }
+            />
+             <input
+              type="text"
+              placeholder="Website"
+              value={editingUser.website}
+              onChange={(e) =>
+                setEditingUser({ ...editingUser, website: e.target.value })
+              }
+            />
+            <button className="btn btn-primary" onClick={handleEditUser}>Save</button>
+            <button className="btn btn-primary" onClick={cancelEdit}>Cancel</button>
+          </div>
+        )}
+      </div>
+              <br></br>
+      <div style={gridStyle}>
+        {posts.map((post) => (
+          <div key={post.id} className="card">
+            <div className="card-body">
+              <h5 className="card-title">Name : {post.name}</h5>
+              <p className="card-text">Username : {post.username}</p>
+              <p className="card-text">Email : {post.email}</p>
+              <p className="card-text">Phone : {post.phone}</p>
+              <p className="card-text">Website : {post.website}</p>
+              <div className="btnt">
+              <button className="btn btn-warning" onClick={() => setEditingUser(post)}>Edit</button>
+              <button className="btn btn-danger" onClick={() => handleDeleteUser(post.id)}>Delete</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      </>
+  );
+}
+
+export default User;
